08d47496123e5220ff15634237e46c302f608064
2pi4rin7 <binhnhan.pahn@gmail.com>
2024-11-28 20:55:32 +0700

    first commit

---
+++ De7/Cau2/time.h
@@ -0,0 +1,42 @@
+#include <iostream>
+class time
+{
+private:
+    int hour, minute, second;
+public:
+    time(int h = 0, int m = 0, int s = 0) :  hour(h), minute(m), second(s) {}
+    friend std::istream& operator>>(std::istream &is, time &t)
+    {
+        is >> t.hour >> t.minute >> t.second;
+        return is;
+    }
+    friend std::ostream& operator<<(std::ostream &os, time t)
+    {
+        os << t.hour << ":" << t.minute << ":" << t.second;
+        return os;
+    }
+    int timetosecond()
+    {
+        int total = hour * 3600 + minute * 60 + second;
+        return total;
+    }
+    time secondtotime(int s)
+    {
+        s = s % 86400;
+        time t;
+        t.hour = s / 3600;
+        t.minute = (s % 3600) / 60;
+        t.second = s % 60;
+        return t;
+    }
+    time operator--()
+    {
+        return secondtotime(this->timetosecond() - 1 + 86400);
+    }
+    time operator--(int)
+    {
+        time t = *this;
+        *this = secondtotime(this->timetosecond() - 1 + 86400);
+        return t;
+    }
+}
